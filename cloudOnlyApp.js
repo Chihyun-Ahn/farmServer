8db58571d723b08740c120f587e1839a224db4cb
Chihyun-Ahn <anchi15@samwooinc.net>
2020-02-21 09:58:36 +0900

    20200221-1

--- cloudOnlyApp.js
+++ cloudOnlyApp.js
@@ -2,10 +2,9 @@
 //#####################################초기화################################################
 
 const express = require('express');
-const cors = require('cors');
 const path = require('path');
 const bodyParser = require('body-parser');
-const cloudApp = express();
+const cloudOnlyApp = express();
 const dbConn = require('./mariadbConn');
 const timeGetter = require('./timeConvert');
 const portNum = 5000;
@@ -16,15 +15,15 @@ var interServerSocket = interServerSocketIO(fogAddress);
 
 var currentUser = 'none';
 
-cloudApp.use(bodyParser.json());
-cloudApp.use(bodyParser.urlencoded({extended: true}));
-cloudApp.use(express.static('views/cssAndpics'));
-cloudApp.use('/socket.io', express.static('node_modules/socket.io'));
-cloudApp.use(cors());
+cloudOnlyApp.use(bodyParser.json());
+cloudOnlyApp.use(bodyParser.urlencoded({extended: true}));
+cloudOnlyApp.use(express.static('views/cssAndpics'));
+cloudOnlyApp.use('/socket.io', express.static('node_modules/socket.io'));
 
 //UI용 소켓 만들기
-var http = require('http').Server(cloudApp);
+var http = require('http').Server(cloudOnlyApp);
 var io = require('socket.io')(http);
+var socketGlobal = 'none';
 
 //###########################################################################################
 //###################################서버 간 소켓 통신#######################################
@@ -40,19 +39,20 @@ interServerSocket.on('house1SyncResFromFog', function(databean){
     console.log('house1SyncResFromFog has arrived. ');
     var msgID = timeGetter.msgIDToMilli(databean.house[0].msgID);
     var msgIDLast, saveParam;
-    // DB 가장 최근 데이터와 시간 차이를 계산, 600초(10분)이상 차이 난다면 저장. 
-    dbConn.getTheLastRow('house1').catch(
-        (err)=>{console.log(err);}
-    ).then(
-        (result)=>{
-            msgIDLast = timeGetter.msgIDToMilli(result[0].msgID);
-            saveParam = msgID - msgIDLast;
-            if(saveParam>=60000){
-                // socketGlobal.emit('cloudMsg', databean);
-                dbConn.insertDataToCloud(databean, 'house1');
-            }
-        }
-    );
+    
+    // // DB 가장 최근 데이터와 시간 차이를 계산, 600초(10분)이상 차이 난다면 저장. 
+    // dbConn.getTheLastRow('house1').catch(
+    //     (err)=>{console.log(err);}
+    // ).then(
+    //     (result)=>{
+    //         msgIDLast = timeGetter.msgIDToMilli(result[0].msgID);
+    //         saveParam = msgID - msgIDLast;
+    //         if(saveParam>=60000){
+    //             // socketGlobal.emit('cloudMsg', databean);
+    //             dbConn.insertDataToCloud(databean, 'house1');
+    //         }
+    //     }
+    // );
 });
 
 interServerSocket.on('house2Msg', function(databean){
@@ -82,25 +82,25 @@ interServerSocket.on('house2SyncResFromFog', function(databean){
 
 //###########################################################################################
 //#################################http GET, POST 통신#######################################
-cloudApp.get('/', function(req, res){
+cloudOnlyApp.get('/', function(req, res){
     console.log('This is the home address.');
     console.log('Redirecting to the login page.');
     res.redirect('/login.do');
 });
 
-cloudApp.get('/login.do', function(req, res){
+cloudOnlyApp.get('/login.do', function(req, res){
     console.log('Entered login page.');
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+    res.sendFile(path.join(__dirname, 'viewsForCloud', 'index.html'));
 });
 
-cloudApp.get('/pastdatapage.do', function(req, res){
-    console.log('cloudApp.get: pastdatapage.do received.');
-    currentUser = req.query.user;
-    console.log('pastdatapage.do: currentUser: '+currentUser);
-    res.sendFile(path.join(__dirname, 'views', 'pastdatapage.html'));
-});
+// cloudOnlyApp.get('/pastdatapage.do', function(req, res){
+//     console.log('cloudOnlyApp.get: pastdatapage.do received.');
+//     currentUser = req.query.user;
+//     console.log('pastdatapage.do: currentUser: '+currentUser);
+//     res.sendFile(path.join(__dirname, 'views', 'pastdatapage.html'));
+// });
 
-cloudApp.post('/loginClick.do', function(req, res){
+cloudOnlyApp.post('/loginClick.do', function(req, res){
     console.log('Log-in button was clicked.');
     var id = req.body.logID;
     var pw = req.body.password;
@@ -110,15 +110,25 @@ cloudApp.post('/loginClick.do', function(req, res){
     dbConn.loginQuery(id, pw).then(function(resultValue){
         if(resultValue == 'No id' || resultValue == 'Wrong password'){
             console.log('아이디/비밀번호가 일치하지 않습니다.');
-            res.sendFile(path.join(__dirname, 'views', 'index.html'));
+            res.sendFile(path.join(__dirname, 'viewsForCloud', 'index.html'));
         }else{
             console.log(resultValue.id+'님 환영합니다. ');
-            res.redirect(fogAddress+'/realdata.do?user='+resultValue.id+'&userLoginClickTime='+userLoginClickTime);
+            res.redirect('/realdata.do?user='+resultValue.id+'&userLoginClickTime='+userLoginClickTime);
         }
     });
 });
 
-cloudApp.post('/setData.do', function(req, res){
+cloudOnlyApp.get('/realdata.do', function(req, res){
+    // console.log('/realdata.do: '+req.query.user);
+    currentUser = req.query.user;
+    userReqTime = req.query.userLoginClickTime;
+    res.sendFile(path.join(__dirname,'viewsForCloud','realtimepage.html'));
+});
+
+
+
+
+cloudOnlyApp.post('/setData.do', function(req, res){
     console.log('setData.do. requested.');
     res.redirect(fogAddress+'/realdata.do');
 });
@@ -139,6 +149,30 @@ io.on('connection', function(socket){
     socket.on('userWho', function(){
         socket.emit('currentUserIs', currentUser);
     });
+    socket.on('house1GraphDataReq', function(){
+        dbConn.getGraphDataset('house1').catch(
+            (err)=>{console.log(err);}
+        ).then(
+            (result)=>{
+                socket.emit('house1GraphDataRes', {result, currentUser, userReqTime});
+            }
+        );
+    });
+
+    socket.on('house2GraphDataReq', function(){
+        dbConn.getGraphDataset('house2').catch(
+            (err)=>{console.log(err);}
+        ).then(
+            (result)=>{
+                socket.emit('house2GraphDataRes', {result, currentUser});
+            }
+        );
+    });
+
+    socket.on('realPageUserReq', function(realPageUserReqTime){
+        socket.emit('realPageUserRes', realPageUserReqTime);
+    });
+
     socket.on('pastGraphDataReq', function(houseName){
         dbConn.getGraphDataset(houseName)
         .catch((err)=>{console.log(err);})
